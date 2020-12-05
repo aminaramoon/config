8becd968cbe0cbd73243359bbca58e94fc1de2c9
Amin Aramoon <amin.aramoon@gmail.com>
2020-12-04 19:26:39 -0800

    (~) removed all the redundant Lmod scripts

--- modules/include_what_you_use/0.11.lua
+++
@@ -1,9 +0,0 @@
-
-local root = '/home/aaramoon/Applications'
-local app = myModuleName()
-local version = myModuleVersion()
-
-local base = pathJoin(root, app, version)
-
-prepend_path('PATH', pathJoin(base, 'bin'))
-
