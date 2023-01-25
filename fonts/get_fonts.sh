3aaaa47e24c09d59280b72e7525503f4a18e76f7
Amin Aramoon <amin.aramoon@gmail.com>
2023-01-24 17:54:43 -0800

    update fonts

---
+++ fonts/get_fonts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "download JetBrains mono font"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
+
+echo "download Inconsolata nerd font"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Inconsolata.zip
+
+
